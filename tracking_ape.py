cfa25c77e643ffa7b53ea85e5dee23c25e44a691
Mateo de Mayo <mateo.demayo@collabora.com>
2022-03-11 12:52:58 -0300

    Implement RPE

--- tracking_ape.py
+++ tracking_ape.py
@@ -9,7 +9,8 @@ from evo.tools.plot import PlotMode
 from evo.core import sync
 from evo.core.trajectory import PoseTrajectory3D
 import evo.main_ape as main_ape
-from evo.core.metrics import PoseRelation
+import evo.main_rpe as main_rpe
+from evo.core.metrics import PoseRelation, Unit
 from evo.tools.settings import SETTINGS
 
 from completion import get_completion_stats, CompletionStats
@@ -19,6 +20,13 @@ from utils import load_trajectory
 def parse_args():
     parser = ArgumentParser(
         description="Determine absolute pose error for a trajectory and its groundtruth",
+    )
+    parser.add_argument(
+        "metric",
+        type=str,
+        help="What tracking metric to compute",
+        default="ate",
+        choices=["ate", "rte"],
     )
     parser.add_argument(
         "tracking_csv",
@@ -42,6 +50,7 @@ def parse_args():
 def get_sanitized_trajectories(
     tracking_csv: Path, groundtruth_csv: Path
 ) -> Tuple[PoseTrajectory3D, PoseTrajectory3D, CompletionStats]:
+    """Trim and synchronizes trajectories so that they have the same amount of poses"""
     # NOTE: Evo uses doubles for its timestamps and thus looses a bit of
     # precision, but even in the worst case, the precision is about ~1usec
     traj_ref = file_interface.read_euroc_csv_trajectory(groundtruth_csv)
@@ -70,7 +79,7 @@ def get_sanitized_trajectories(
     return traj_est, traj_ref, c
 
 
-def get_tracking_stats(
+def get_tracking_ape_stats(
     tracking_csv: Path,
     groundtruth_csv: Path,
     pose_relation: PoseRelation = PoseRelation.translation_part,
@@ -112,14 +121,71 @@ def get_tracking_stats(
     return result, c
 
 
+def get_tracking_rpe_stats(
+    tracking_csv: Path,
+    groundtruth_csv: Path,
+    pose_relation: PoseRelation = PoseRelation.translation_part,
+    show_plot: bool = False,
+):
+    traj_est, traj_ref, c = get_sanitized_trajectories(tracking_csv, groundtruth_csv)
+    ref_name = "reference"
+    est_name = "estimated"
+    umemaya_align = True  # Otherwise align_origin
+    result = main_rpe.rpe(
+        traj_ref=traj_ref,
+        traj_est=traj_est,
+        pose_relation=pose_relation,  # TODO: I might want to also consider rotation_angle_deg? ROE?
+        delta=6,
+        delta_unit=Unit.frames,  # TODO: Evo doesn't support delta_units=seconds, I think 0.2s would be good
+        rel_delta_tol=0.1,  # TODO: This only seems to be used when all_pairs is enabled
+        all_pairs=False,  # TODO: Use all_pairs?
+        align=umemaya_align,
+        correct_scale=False,
+        n_to_align=-1,
+        align_origin=umemaya_align,  # TODO: Doesn't this make a lot of sense for VR?
+        ref_name=ref_name,
+        est_name=est_name,
+        support_loop=False,  # Seems to only be used to not modify the input trajectories in jupyter notebooks
+    )
+
+    if show_plot:
+        import matplotlib.pyplot as plt
+
+        fig = plt.figure()
+        plot_mode = PlotMode.xyz
+        ax = plot.prepare_axis(fig, plot_mode)
+        plot.traj(ax, plot_mode, traj_ref, style="--", alpha=0.5)
+        plot.traj_colormap(
+            ax,
+            result.trajectories[est_name],
+            result.np_arrays["error_array"],
+            plot_mode,
+            min_map=result.stats["min"],
+            max_map=result.stats["max"],
+        )
+        plt.show()
+
+    return result, c
+
+
 def main():
     args = parse_args()
+    metric = args.metric
     tracking_csv = args.tracking_csv
     groundtruth_csv = args.groundtruth_csv
     show_plot = args.plot
-    result, completion = get_tracking_stats(
-        tracking_csv, groundtruth_csv, show_plot=show_plot
-    )
+
+    if metric == "ate":
+        result, completion = get_tracking_ape_stats(
+            tracking_csv, groundtruth_csv, show_plot=show_plot
+        )
+    elif metric == "rte":
+        result, completion = get_tracking_rpe_stats(
+            tracking_csv, groundtruth_csv, show_plot=show_plot
+        )
+    else:
+        assert False
+
     print(result)
     print(completion)
 

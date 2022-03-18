4a676e3a644767445f705a02f62f2cdefba9dd58
Mateo de Mayo <mateo.demayo@collabora.com>
2022-03-18 13:37:20 -0300

    Refactor dataset iteration into its won function

--- batch.py
+++ batch.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-import csv
-from typing import List
-from os import listdir, stat
-from os.path import exists
+from typing import Callable, Optional, Union
 from tabulate import tabulate
 from timing import load_timing_stats
 from completion import load_completion_stats
@@ -15,119 +12,77 @@ import pandas as pd
 DIVIDE_BY = 1000000  # TODO: Use --units (make it a parent argparser)
 EVALUATION_PATH = Path("runs")
 # EVALUATION_PATH = Path("runs-max-speed")
-GT_PATH = Path("gts")
+TARGETS_PATH = Path("targets")
 INDICES = {"K": (3, 4), "B": (4, 11), "O": (4, 5)}
 UNITS = "ms"
 
+SimpleMeasureF = Callable[[Path, str], str]
+TargetMeasureF = Callable[[Path, Path], str]
+MeasureFunction = Union[SimpleMeasureF, TargetMeasureF]
 
-def get_all_dataset_paths(eval_path: str) -> List[str]:
-    return [
-        f"{eval_path}/{system}/{dataset}"
-        for system in listdir(f"{eval_path}/")
-        for dataset in listdir(f"{eval_path}/{system}")
-    ]
+
+def foreach_dataset(
+    result_fn: str, target_fn: Optional[str], measure_str  #: MeasureFunction
+):
+    sys_dirs = [r for r in EVALUATION_PATH.iterdir() if r.is_dir()]
+    ordered_set = {d.name: 0 for r in sys_dirs for d in r.iterdir() if d.is_dir()}
+    ds_names = sorted(ordered_set.keys())
+    sys_names = sorted([d.name for d in sys_dirs])
+    df = pd.DataFrame("—", columns=sys_names, index=ds_names)
+
+    for sys_dir in sys_dirs:
+        for ds_name in ds_names:
+            ds_dir = sys_dir / ds_name
+            if ds_dir.is_dir():
+                sys_name = sys_dir.name
+                result_csv = ds_dir / result_fn
+                if result_csv.exists() and result_csv.stat().st_size != 0:
+                    if target_fn is None:
+                        df[sys_name][ds_name] = measure_str(result_csv, sys_name)
+                    else:
+                        target_csv = TARGETS_PATH / ds_name / target_fn
+                        if target_csv.exists():
+                            df[sys_name][ds_name] = measure_str(result_csv, target_csv)
+
+    print(tabulate(df, headers="keys", tablefmt="pipe"))
 
 
 def timing_main():
-    EXPECTED_CSV_FILE = "timing.csv"
-    sys_dirs = [r for r in EVALUATION_PATH.iterdir() if r.is_dir()]
-    ds_names = {d.name: 0 for r in sys_dirs for d in r.iterdir() if d.is_dir()}
-    ds_names = sorted(ds_names.keys())
-    sys_names = sorted([d.name for d in sys_dirs])
-    df = pd.DataFrame("—", columns=sys_names, index=ds_names)
+    def measure_timing(result_csv: Path, sys_name: str) -> str:
+        i, j = INDICES[sys_name[0]]
+        s = load_timing_stats(result_csv, i=i, j=j)
+        return f"{s.mean:.2f} ± {s.std:.2f}"
 
-    for sys_dir in sys_dirs:
-        for ds_name in ds_names:
-            ds_dir = sys_dir / ds_name
-            if ds_dir.is_dir():
-                sys_name = sys_dir.name
-                csvfile = ds_dir / EXPECTED_CSV_FILE
-                if csvfile.exists() and csvfile.stat().st_size != 0:
-                    ### TODO: Generalize the rest
-                    i, j = INDICES[sys_name[0]]
-                    s = load_timing_stats(csvfile, i=i, j=j)
-                    ### TODO: Generalize the rest
-                    df[sys_name][ds_name] = f"{s.mean:.2f} ± {s.std:.2f}"
-
-    print(tabulate(df, headers="keys"))
+    foreach_dataset("timing.csv", None, measure_timing)
 
 
 def completion_main():
-    EXPECTED_CSV_FILE = "tracking.csv"
-    sys_dirs = [r for r in EVALUATION_PATH.iterdir() if r.is_dir()]
-    ds_names = {d.name: 0 for r in sys_dirs for d in r.iterdir() if d.is_dir()}
-    ds_names = sorted(ds_names.keys())
-    sys_names = sorted([d.name for d in sys_dirs])
-    df = pd.DataFrame("—", columns=sys_names, index=ds_names)
+    def measure_completion(result_csv: Path, target_csv: Path) -> str:
+        s = load_completion_stats(result_csv, target_csv)
+        r = (
+            f"{s.tracking_completion * 100:.2f}%"
+            if s.tracking_completion < 0.98
+            else "✓"
+        )
+        return r
 
-    for sys_dir in sys_dirs:
-        for ds_name in ds_names:
-            ds_dir = sys_dir / ds_name
-            if ds_dir.is_dir():
-                sys_name = sys_dir.name
-                csvfile = ds_dir / EXPECTED_CSV_FILE
-                if csvfile.exists() and csvfile.stat().st_size != 0:
-                    ### TODO: Generalize the rest
-                    frames_csv = GT_PATH / ds_name / "cam0.csv"
-                    if frames_csv.exists():
-                        s = load_completion_stats(csvfile, frames_csv)
-                        ### TODO: Generalize the rest
-                        # df[sys_name][ds_name] = f"{s.tracking_completion * 100:.2f}%"
-                        df[sys_name][ds_name] = (
-                            f"{s.tracking_completion * 100:.2f}%"
-                            if s.tracking_completion < 0.98
-                            else "✓"
-                        )
-    print(tabulate(df, headers="keys"))
+    foreach_dataset("tracking.csv", "cam0.csv", measure_completion)
 
 
 def ate_main():
-    EXPECTED_CSV_FILE = "tracking.csv"
-    sys_dirs = [r for r in EVALUATION_PATH.iterdir() if r.is_dir()]
-    ds_names = {d.name: 0 for r in sys_dirs for d in r.iterdir() if d.is_dir()}
-    ds_names = sorted(ds_names.keys())
-    sys_names = sorted([d.name for d in sys_dirs])
-    df = pd.DataFrame("—", columns=sys_names, index=ds_names)
+    def measure_ape(result_csv: Path, target_csv: Path) -> str:
+        s = get_tracking_ape_stats(result_csv, target_csv)[0].stats
+        return f"{s['mean']:.3f} ± {s['std']:.3f}"
 
-    for sys_dir in sys_dirs:
-        for ds_name in ds_names:
-            ds_dir = sys_dir / ds_name
-            if ds_dir.is_dir():
-                sys_name = sys_dir.name
-                csvfile = ds_dir / EXPECTED_CSV_FILE
-                if csvfile.exists() and csvfile.stat().st_size != 0:
-                    ### TODO: Generalize the rest
-                    gt_csv = GT_PATH / ds_name / "gt.csv"
-                    if gt_csv.exists():
-                        s = get_tracking_ape_stats(csvfile, gt_csv)[0].stats
-                        ### TODO: Generalize the rest
-                        df[sys_name][ds_name] = f"{s['mean']:.3f} ± {s['std']:.3f}"
+    foreach_dataset("tracking.csv", "gt.csv", measure_ape)
 
-    print(tabulate(df, headers="keys"))
 
 def rte_main():
-    EXPECTED_CSV_FILE = "tracking.csv"
-    sys_dirs = [r for r in EVALUATION_PATH.iterdir() if r.is_dir()]
-    ds_names = {d.name: 0 for r in sys_dirs for d in r.iterdir() if d.is_dir()}
-    ds_names = sorted(ds_names.keys())
-    sys_names = sorted([d.name for d in sys_dirs])
-    df = pd.DataFrame("—", columns=sys_names, index=ds_names)
+    def measure_rpe(result_csv: Path, target_csv: Path) -> str:
+        s = get_tracking_rpe_stats(result_csv, target_csv)[0].stats
+        return f"{s['mean']:.3f} ± {s['std']:.3f}"
 
-    for sys_dir in sys_dirs:
-        for ds_name in ds_names:
-            ds_dir = sys_dir / ds_name
-            if ds_dir.is_dir():
-                sys_name = sys_dir.name
-                csvfile = ds_dir / EXPECTED_CSV_FILE
-                if csvfile.exists() and csvfile.stat().st_size != 0:
-                    ### TODO: Generalize the rest
-                    gt_csv = GT_PATH / ds_name / "gt.csv"
-                    if gt_csv.exists():
-                        s = get_tracking_rpe_stats(csvfile, gt_csv)[0].stats
-                        ### TODO: Generalize the rest
-                        df[sys_name][ds_name] = f"{s['mean']:.3f} ± {s['std']:.3f}"
-
-    print(tabulate(df, headers="keys"))
+    foreach_dataset("tracking.csv", "gt.csv", measure_rpe)
 
 
 def main():
